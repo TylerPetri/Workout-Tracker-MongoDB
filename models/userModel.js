9d201112eb63507b34b8a3ee3564abd820b1aa68
T__R__P__53 <tyler_1701@hotmail.com>
2021-03-15 16:41:51 -0400

    work

--- models/userModel.js
+++ models/userModel.js
@@ -35,6 +35,6 @@ const UserSchema = new Schema({
     ]
 });
 
-const Workout = mongoose.model("Workout", UserSchema);
+const Workout = mongoose.model("workout", UserSchema);
 
 module.exports = Workout;
